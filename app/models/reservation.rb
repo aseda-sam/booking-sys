3cd6ba9b26a5a0a29d73eadf6d8c2dcdcc433958
Aseda Sam <aoheneadu23@gmail.com>
2018-09-27 08:57:12 +0000

    Start and End time validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -8,4 +8,22 @@ class Reservation < ApplicationRecord
   
   validates :group, presence: true
   validates :activity, presence: true
+  
+  #validates :reserve_date, presence: true, date: { after_or_equal_to: Proc.new { Date.today }, message: "must be at least #{(Date.today + 1).to_s}" }, on: :create
+  # validates :start_date, presence: true, date: { after_or_equal_to: Proc.new { Date.today }, message: "must be at least #{(Date.today + 1).to_s}" }, on: :create
+  # validates :reserve_date, presence: true
+  
+  validates_presence_of :time_start, :time_end
+  validate :time_end_is_after_time_start
+  
+  
+  private
+    def time_end_is_after_time_start
+      return if time_end.blank? || time_start.blank?
+    
+      if time_end < time_start
+        errors.add(:time_end, "cannot be before the start time") 
+      end 
+    end
+
 end
