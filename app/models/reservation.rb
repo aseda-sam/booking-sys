0c68babe458f285d02ef1eeb4f95d9acf52c1f0e
Aseda Sam <aoheneadu23@gmail.com>
2018-09-27 08:23:22 +0000

    validated rsv.email with regex + downcase && room.name

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,11 @@
 class Reservation < ApplicationRecord
   belongs_to :room
+  
+  before_save { self.email = email.downcase }
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, length: { maximum: 105 }, format: {with: VALID_EMAIL_REGEX }
+  
+  validates :group, presence: true
+  validates :activity, presence: true
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,4 @@
 class Room < ApplicationRecord
   has_many :reservations
+  validates :name, uniqueness: { case_sensitive: false }
 end
