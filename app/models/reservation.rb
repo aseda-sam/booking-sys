50645ecdf3679b04914e173c4218ec00f362b108
Aseda Sam <aoheneadu23@gmail.com>
2018-09-28 12:02:24 +0000

    Fixed navbar responsiveness as well as some other components

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,10 +5,11 @@ class Reservation < ApplicationRecord
   
   belongs_to :room
   
-  # Email Validations
+  # Email Validations. See https://stackoverflow.com/questions/6567793/validate-email-format-only-if-not-blank-rails-3 for more ideas
   before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, length: { maximum: 105 }, format: {with: VALID_EMAIL_REGEX }
+  validates :email, :allow_blank => true, length: { maximum: 105 }, format: {with: VALID_EMAIL_REGEX }
+  # validates :email,:presence => {:message => "Your email is used to save your greeting."}, :allow_blank => true,:uniqueness => { :case_sensitive => false }
   
   # Other Validations
   validates :group, presence: true
