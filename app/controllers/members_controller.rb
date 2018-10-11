bb5a7d292ef118737ee0430c1cb4d59f583b7d5f
Aseda Sam <aoheneadu23@gmail.com>
2018-10-11 12:47:01 +0000

    No auth if creating member

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,5 @@
 class MembersController < ApplicationController
+  before_action :authenticate_user!, except: [:new, :edit,:create]
   before_action :set_member, only: [:show, :edit, :update, :destroy]
 
   # GET /members
