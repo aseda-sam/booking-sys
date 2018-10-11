860b0920253de69ae35c5d2a1647778d2a7fa3f9
Aseda Sam <aoheneadu23@gmail.com>
2018-10-11 10:32:07 +0000

    Devise installed and working

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   
+  root to: 'reservations#index'
+  
+  devise_for :users
   resources :reservations
   resources :rooms
-  
-  
-  root to: 'reservations#index'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
